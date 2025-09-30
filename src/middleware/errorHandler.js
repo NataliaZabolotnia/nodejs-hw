8ab218a78247280ef1b5b68802634c603b6c88a9
Natalia Zabolotnia <zanatav78@gmail.com>
2025-09-30 09:22:29 +0300

    change errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,12 @@
+import { HttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
-  console.error(err.message);
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message || err.name,
+    });
+  }
+  console.error(err);
   res.status(500).json({
     message: err.message,
   });
