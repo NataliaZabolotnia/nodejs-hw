3902374af1690c78863c8ffe78244fe5d55af0d8
Natalia Zabolotnia <zanatav78@gmail.com>
2025-10-04 18:25:54 +0300

    initial

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,13 +1,34 @@
 import { Note } from '../models/note.js';
 import createHttpError from 'http-errors';
+// import { Joi, Segments } from "celebrate";
 
 export const getAllNotes = async (req, res) => {
-  const notes = await Note.find();
-  res.status(200).json(notes);
+  const { tag, search, page = 1, perPage = 10 } = req.query;
+  const skip = (page - 1) * perPage;
+  let notesQuery = Note.find();
+  if (tag) {
+    notesQuery.where('tag').equals(tag);
+  }
+
+  if (search) {
+    notesQuery.or([
+      { title: { $regex: search, $options: 'i' } },
+      { content: { $regex: search, $options: 'i' } },
+    ]);
+  }
+  const [totalNotes, notes] = await Promise.all([
+    notesQuery.clone().countDocuments(),
+    notesQuery.skip(skip).limit(perPage),
+  ]);
+  const totalPage = Math.ceil(totalNotes / perPage);
+  // const notes = await notesQuery;
+
+  res.status(200).json({ page, perPage, totalNotes, totalPage, notes });
 };
 export const getNoteById = async (req, res, next) => {
   const { noteId } = req.params;
   const note = await Note.findById(noteId);
+
   if (!note) {
     next(createHttpError(404, 'Note not found'));
     return;
--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import { logger } from './middleware/logger.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
-
+import { errors } from 'celebrate';
 const app = express();
 const PORT = process.env.PORT ?? 3030;
 
@@ -34,6 +34,7 @@ app.use(
 );
 app.use(notesRoutes);
 app.use(notFoundHandler);
+app.use(errors());
 app.use(errorHandler);
 
 await connectMongoDB();
